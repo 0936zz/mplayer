fbe986d4d6d085ff7e36db0c894cfb889c863d8c
Unknown <zz5840@qq.com>
2017-01-01 23:50:01 +0800

    V2.0.0
    新增pjax跳转

--- code/start-create.js
+++ code/start-create.js
@@ -1,23 +1,24 @@
-new Mplayer({
-	// 盛放mPlayer的父容器
-	containerSelector:'.container',
-	// 歌曲列表
-	songList:[
-		[
-			{
-				"basic":true,
-				"name":"专辑名/列表名",
-				"singer":"列表公用歌手",
-				"img":"列表公用图片"
-			},
-			{
-				"name":"歌名",
-				"img":"本歌曲图片",
-				"src":"歌曲路径",
-				"lrc":"lrc歌词,每句用'\n'隔开"
-			}
-			// 可以添加更多歌曲
-		]
-		// 可以添加更多列表
-	]
+new MPlayer({
+	// 容器选择器名称
+	containerSelector: '.mp',
+	// 播放列表，格式请参考mplayer-list.js
+	songList: mplayer_song,
+	// 专辑图片错误时显示的图片
+	defaultImg: 'img/mplayer_error.png',
+	// 自动播放
+	autoPlay: false,
+	// 播放模式(0->顺序播放,1->单曲循环,2->随机播放,3->列表循环(默认))
+	playMode:0,
+	// 第一首播放的列表
+	playList:0,
+	// 第一首播放的歌曲
+	playSong:5,
+	// 当前歌词距离顶部的距离
+	lrcTopPos: 34,
+	// 列表模板，用${变量名}$插入模板变量
+	listFormat: '<tr><td>${name}$</td><td>${singer}$</td><td>${time}$</td></tr>',
+	// 音量滑块改变事件名称
+	volSlideEventName:'change',
+	// 初始音量
+	defaultVolume:80
 });
